4d0dab8f93cca005e6d3f9a2f5f16e56b49847f5
fsclaudio <fsclaudio@gmail.com>
2021-07-26 00:42:35 -0300

    create table genre

--- backend/src/main/resources/Data.sql
+++ backend/src/main/resources/Data.sql
@@ -7,3 +7,9 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+
+INSERT INTO tb_genre (name) VALUES ('Anime');
+INSERT INTO tb_genre (name) VALUES ('Action');
+INSERT INTO tb_genre (name) VALUES ('Suspense');
+INSERT INTO tb_genre (name) VALUES ('Serie');
+INSERT INTO tb_genre (name) VALUES ('Terror');
