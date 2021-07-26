526e56e3ed9eb0433182ca0f3f108770bfddf11a
fsclaudio <fsclaudio@gmail.com>
2021-07-26 01:39:19 -0300

    Create table Movie and mapping

--- backend/src/main/java/com/cfs/movieflix/entities/Genre.java
+++ backend/src/main/java/com/cfs/movieflix/entities/Genre.java
@@ -1,11 +1,14 @@
 package com.cfs.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +21,9 @@ public class Genre implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@OneToMany(mappedBy = "genre")
+	private List<Movie> movies = new ArrayList<>();
 	
 	public Genre() {
 	}
@@ -41,6 +47,12 @@ public class Genre implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	
+	
+	public List<Movie> getMovies() {
+		return movies;
 	}
 
 	@Override
--- backend/src/main/resources/Data.sql
+++ backend/src/main/resources/Data.sql
@@ -11,5 +11,10 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_genre (name) VALUES ('Anime');
 INSERT INTO tb_genre (name) VALUES ('Action');
 INSERT INTO tb_genre (name) VALUES ('Suspense');
-INSERT INTO tb_genre (name) VALUES ('Serie');
+INSERT INTO tb_genre (name) VALUES ('Aventura');
 INSERT INTO tb_genre (name) VALUES ('Terror');
+INSERT INTO tb_genre (name) VALUES ('Drama');
+
+INSERT INTO tb_movie (title,sub_Title,year,img_Url,synopsis,genre_id) VALUES ('News of The World','Relatos do Mundo',2021,'https://br.web.img3.acsta.net/c_310_420/pictures/21/01/28/19/17/1791660.jpg','Em Relatos do Mundo, no ano de 1870, o Capitão Jefferson Kyle Kidd, um viúvo que já lutou em duas guerras, viaja através do Texas oferecendo notícias do mundo para as pessoas, apesar dos jornais estarem se tornando cada vez mais acessíveis.',4);
+INSERT INTO tb_movie (title,sub_Title,year,img_Url,synopsis,genre_id) VALUES (' Slumdog Millionaire','Quem quer ser um milionario',2008,'https://br.web.img2.acsta.net/c_310_420/medias/nmedia/18/87/18/75/20028641.jpg','Jamal Malik vem de uma família das favelas de Mumbai, Índia, e está prestes a ganhar o prêmio de 20 milhões de rúpias no programa Quem Quer Ser Um Milionário?- feito que nenhum participante jamais conseguira até então.',6);
+INSERT INTO tb_movie (title,sub_Title,year,img_Url,synopsis,genre_id) VALUES (' Luca','Luca',2021,'https://br.web.img2.acsta.net/c_310_420/pictures/21/05/07/10/59/3500748.jpg','Em Luca, acompanhamos uma história de amadurecimento sobre um jovem que vive um verão inesquecível repleto de sorvetes, massas e passeios intermináveis de scooter.',1);
